ec0e59ec7c68f3911359cc522df599a1013d3355
Mudit Gurjar <mudit182@gmail.com>
2023-03-25 01:32:55 +0530

    - added a sample decorator for optimizing selectors in page object files (currently not functional as decorators are not supported by playwright)

---
+++ playwright/test-factory-utils/interactive-web-element-decorator.ts
@@ -0,0 +1,28 @@
+import { InteractiveWebElement } from "./interactive-web-element";
+
+export function interactiveWebElement(label: string, selector: string) {
+  return function (target: any, propertyKey: string) {
+    Object.defineProperty(target, propertyKey, {
+      get: function () {
+        return new InteractiveWebElement(
+          label,
+          selector,
+          this.page,
+          this.userName
+        );
+      },
+    });
+
+    Object.defineProperty(target.constructor, `${propertyKey}Label`, {
+      value: label,
+      writable: false,
+      configurable: false,
+    });
+
+    Object.defineProperty(target.constructor, `${propertyKey}Selector`, {
+      value: selector,
+      writable: false,
+      configurable: false,
+    });
+  };
+}
