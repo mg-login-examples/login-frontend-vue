fc169db73a1c37d08f21fe1eaa2e5c16d0ebccf5
Mudit Gurjar <mudit182@gmail.com>
2024-04-04 11:06:01 +0530

    - update frontend packages, synk config with new vue projects

--- src/utils/envUtils/__tests__/index.spec.ts
+++ src/utils/envUtils/__tests__/index.spec.ts
@@ -1,12 +1,12 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect } from 'vitest'
 
-import { EnvironmentVars } from "@/utils/envUtils";
+import { EnvironmentVars } from '@/utils/envUtils'
 
-describe("Utils > envUtils > index.ts", () => {
-  it("checks development environment", () => {
-    import.meta.env.DEV = false;
-    expect(EnvironmentVars.isDevelopmentEnvironment).toBe("false");
-    import.meta.env.DEV = true;
-    expect(EnvironmentVars.isDevelopmentEnvironment).toBe("true");
-  });
-});
+describe('Utils > envUtils > index.ts', () => {
+  it('checks development environment', () => {
+    import.meta.env.DEV = false
+    expect(EnvironmentVars.isDevelopmentEnvironment).toBe(false)
+    import.meta.env.DEV = true
+    expect(EnvironmentVars.isDevelopmentEnvironment).toBe(true)
+  })
+})
